2ef8a2c8f6e1a8f8471dd11bf360b9bbde999941
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-06-23 09:57:27 -0700

    Time: 110 ms (60.90%), Space: 44.5 MB (44.07%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -7,21 +7,15 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) {
         return false;
     }
-    const arr1 = s.split('');
-    const arr2 = t.split('');
     const map = {};
-    
-    for (let c of arr1) {
+    for (let c of s) {
         map[c] = (map[c] || 0) + 1;
     }
-    
-    for (let e of arr2) {
+    for (let e of t) {
         if (!map[e]) {
             return false;
         }
         map[e]--;
     }
-    
     return true;
-
 };
