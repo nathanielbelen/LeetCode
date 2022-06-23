e2c240063182ba7a68a462933bc137f0b69cadb8
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-06-23 13:50:12 -0700

    Time: 101 ms (66.02%), Space: 43.1 MB (31.84%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    const map = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        const num = nums[i]
+        if (map[num] !== undefined) {
+            return [i, map[num]];
         }
+        map[target-num] = i;
     }
-    return null;
 };
