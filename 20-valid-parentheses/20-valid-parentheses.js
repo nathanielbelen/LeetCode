39b039188dd2bec5429785181dc515875062cf59
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-06-23 14:10:28 -0700

    Time: 76 ms (69.76%), Space: 42.5 MB (33.71%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -1,45 +1,33 @@
-/*
- @param {string} s
- @return {boolean}
-*/
-var isValid = function (s) {
-  let open = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
-      open.unshift(s[i]);
-    } else if (s[i] === ')' || s[i] === '}' || s[i] === ']') {
-      if (isMatching(s[i], open[0])) {
-        open.shift();
-      } else {
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    for (let c of s) {
+        if (c === '(' || c === '[' || c === '{') {
+            if (c === '(') {
+                stack.unshift(')')
+            } else if (c === '[') {
+                stack.unshift(']')
+            } else if (c === '{') {
+                stack.unshift('}')
+            }
+        } else if (c === ')' || c === ']' || c === '}') {
+            if (c === ')' && stack[0] === ')') {
+                stack.shift();
+            } else if (c === ']' && stack[0] === ']') {
+                stack.shift();
+            } else if (c === '}' && stack[0] === '}') {
+                stack.shift();
+            } else {
+                return false;
+            }
+        }
+    }
+    if (!stack.length) {
+        return true;
+    } else {
         return false;
-      }
     }
-  }
-  if (open.length === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-let isMatching = (closed, open) => {
-  if (closed === ')') {
-    if (open !== '(') {
-      return false;
-    }
-    return true;
-  }
-  if (closed === ']') {
-    if (open !== '[') {
-      return false;
-    }
-    return true;
-  }
-  if (closed === '}') {
-    if (open !== '{') {
-      return false;
-    }
-    return true;
-  }
-  return false;
 };
