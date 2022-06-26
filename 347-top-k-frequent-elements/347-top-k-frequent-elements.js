0d2f6464aeff5cf1397153d751818763c1307d60
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-06-26 15:53:31 -0700

    Time: 400 ms (5.00%), Space: 50.8 MB (7.84%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map = {};
+    nums.forEach((num) => {
+        console.log(num.toString())
+        if (map[num] === undefined) {
+            map[num] = 0;
+        } else {
+            map[num]++;
+        }
+    })
+    
+    let frequency = [];
+    for (let number in map) {
+        frequency.push([Number(number), map[number]])
+    }
+    frequency.sort(function(a, b) {
+        return b[1] - a[1]
+    })
+    console.log(frequency)
+    answer = [];
+    for (let i = 0; i < k; i++) {
+        answer.push(parseInt(frequency[i]));
+    }
+    return answer;
+};
