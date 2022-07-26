8af29e7d47d41ec5b7342af60ec129b1eccf47a9
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-07-26 11:04:30 -0700

    Time: 104 ms (35.43%), Space: 45.3 MB (21.17%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,23 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  // nums.length - 1 = last index
-  // [0, 1, 2, 3, 4, 5, 6]
-  // pick middle index
-  // if target < middle index, it's within 0 and middle index - 1
-  // >, between middle index + 1 and max
-  // recurse
   let low = 0;
-  let high = nums.length - 1;
+  let high = nums.length - 1
   while (low <= high) {
-    let middle = nums[Math.floor((low+high)/2)]
-    if (middle === target) {
-      return Math.floor((low+high)/2);
-    } else if (middle < target) {
-      low = Math.floor((low+high)/2) + 1;
-    } else if (middle > target) {
-      high = Math.floor((low+high)/2) - 1;
+    let mid = Math.floor((high + low)/2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
-  return -1
+  return -1;
 };
