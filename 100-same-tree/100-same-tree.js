c7c2e65f45b04bc57cdffff8e1378a7665c43f1f
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-07-28 14:30:32 -0700

    Time: 87 ms (48.68%), Space: 42.1 MB (71.40%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,49 +12,10 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    let identical = true;
-    const search = (node1, node2) => {
-        
-        if ((!node1 && node2) || (node1 && !node2)) {
-            identical = false;
-            return;
-        }
-        
-        else if (!node1 && !node2) {
-            return
-        }
-        
-        search(node1.left, node2.left);
-        search(node1.right, node2.right);
-        
-        if (node1.val !== node2.val) {
-            identical = false;
-            return
-        }
-        
-        return;
-        
-    };
+    if (p === q) return true;
+    if (p === null && q !== null) return false;
+    if (p !== null && q === null) return false;
+    if (p.val !== q.val) return false;
     
-    search(p, q);
-    return identical;
-    
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
-
-// set identical variable = true;
-// dfs tree1, tree2
-//     if (!tree1 && tree2 || tree1 && !tree2)
-//         identical = false;
-//         return;
-//     else if (both nodes dont exist) {
-//         return;
-//     }
-//     if (node1.val !== node2.val) {
-//         identical = false;
-//     }
-//     dfs(tree1.left, tree2.left)
-//     dfs(tree1.right, tree2.right)
-//     return 
-
-// dfs(tree1, tree2)
-// return identical;
