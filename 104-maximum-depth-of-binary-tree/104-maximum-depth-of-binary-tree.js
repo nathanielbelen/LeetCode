a5f320b3472af8d6803f81d15f641e9c2ef4497f
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-07-28 11:03:03 -0700

    Time: 81 ms (80.18%), Space: 45.3 MB (52.41%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,24 +11,30 @@
  * @return {number}
  */
 
-var maxDepth = function(root) {
-    const queue = [];
-    if (root !== null) {
-        queue.push({node: root, depth: 1})
+maxDepth = function(root) {
+    if (root === null) {
+        return 0;
     }
-    let curr;
-    let maxDepth = 0;
-    while (queue.length) {
-        curr = queue.shift();
-        if (curr.node !== null) {
-             if (curr.node.left !== null) {
-                queue.push({node: curr.node.left, depth: curr.depth + 1});
-            }
-            if (curr.node.right !== null) {
-                queue.push({node: curr.node.right, depth: curr.depth + 1});
-            }   
-        }
-        maxDepth = curr.depth > maxDepth ? curr.depth : maxDepth;
-    }
-    return maxDepth;
-};
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
+
+
+// var maxDepth = function(root) {
+//     if (!root) return 0;
+//     const queue = [{node: root, depth: 1}];
+//     let curr;
+//     let maxDepth = 0;
+//     while (queue.length) {
+//         curr = queue.shift();
+//         if (curr.node !== null) {
+//              if (curr.node.left !== null) {
+//                 queue.push({node: curr.node.left, depth: curr.depth + 1});
+//             }
+//             if (curr.node.right !== null) {
+//                 queue.push({node: curr.node.right, depth: curr.depth + 1});
+//             }   
+//         }
+//         maxDepth = curr.depth > maxDepth ? curr.depth : maxDepth;
+//     }
+//     return maxDepth;
+// };
