35edda5c6c73432e2daef7fc01419499eb9ec54f
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-08-02 01:13:10 -0700

    Time: 69 ms (84.85%), Space: 42.2 MB (70.31%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -11,19 +11,34 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    // shift remove from front of array
-    // push add to back
-    let queue = [root];
-    let holder;
-    let curr;
-    while (queue.length) {
-        curr = queue.shift();
-        if (curr !== null) {
-            holder = curr.left;
-            curr.left = curr.right;
-            curr.right = holder;
-            queue.push(curr.left, curr.right);   
-        }
+    const dfs = (node) => {
+        if (!node) return;
+        let holder = node.right;
+        node.right = node.left;
+        node.left = holder;
+        dfs(node.left)
+        dfs(node.right)
+        return;
     }
+    dfs(root)
     return root;
 };
+
+// original iterative solution
+// var invertTree = function(root) {
+//     // shift remove from front of array
+//     // push add to back
+//     let queue = [root];
+//     let holder;
+//     let curr;
+//     while (queue.length) {
+//         curr = queue.shift();
+//         if (curr !== null) {
+//             holder = curr.left;
+//             curr.left = curr.right;
+//             curr.right = holder;
+//             queue.push(curr.left, curr.right);   
+//         }
+//     }
+//     return root;
+// };
