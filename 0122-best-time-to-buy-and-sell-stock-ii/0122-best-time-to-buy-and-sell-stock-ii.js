e9549a921ca98f4e5678d48e0c181531520dd999
Nathaniel Belen <nathaniel.belen@gmail.com>
2023-06-06 01:12:24 -0700

    Time: 46 ms (99.35%), Space: 41.9 MB (87.42%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,26 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let low = Infinity;
-    let high = 0;
     let profit = 0;
+    let min = Infinity;
     
     for (let i = 0; i < prices.length; i++) {
-        let curr = prices[i];
-
-        if (curr < high) {
-            profit += high - low;
-            low = curr;
-            high = 0;
-        } else if (curr < low) {
-            low = curr;
-        } else if (curr > high) {
-            high = curr;
+        if (prices[i] < min) {
+            min = prices[i]
+        } else if (prices[i] > min) {
+            profit += prices[i] - min;
+            min = prices[i];
         }
-    }
-    
-    if (high) {
-        profit += high - low;
     }
     
     return profit;
